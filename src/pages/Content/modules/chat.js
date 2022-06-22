13c243093ce49185dfdf7a31ddbeba70e6b59f09
EJKim <dmdwn3979@gmail.com>
2022-06-22 22:19:55 +0900

    Add: 버튼 asset 추가

--- src/pages/Content/modules/chat.js
+++ src/pages/Content/modules/chat.js
@@ -1,22 +1,12 @@
 import React from "react";
 import { render } from 'react-dom';
-import Chatting from './component/Chatting'
+import ViewButton from './component/ViewButton/index'
 
 export const createChatDiv = () => {
     const ele = document.createElement('div');
     ele.id = 'chatRoomDiv';
-    ele.style.position = 'fixed';
-    ele.style.display = 'none';
-    ele.style.height = '800px';
-    ele.style.width = '400px';
-    ele.style.right = '50px';
-    ele.style.top = '50px';
-    ele.style.backgroundColor = 'white';
-    ele.style.border = '1px solid #eef2f4';
-    ele.style.zIndex = 9999;
-    ele.style.display = 'inline-block';
     document.body.appendChild(ele);
-    render(<Chatting />, window.document.querySelector('#chatRoomDiv'));
+    render(<ViewButton />, window.document.querySelector('#chatRoomDiv'));
 }
 
 
